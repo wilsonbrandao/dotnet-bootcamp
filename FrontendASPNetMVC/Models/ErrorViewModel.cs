e623cfa31832678221f60d69d5909c914e008aff
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2022-12-10 17:03:46 -0300

    create project dotnet mcv

---
+++ FrontendASPNetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FrontendASPNetMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
